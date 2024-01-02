753d0dc196979361a8c78b74560583eafb4f161b
skhacker105 <skhacker105@gmail.com>
2024-01-02 22:15:24 +0530

    fixed routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch:'full',
+        redirectTo: 'login'
+    },
+    {
+        path: 'login',
+        loadChildren: () => loadRemoteModule({
+            type: 'module',
+            remoteEntry: 'http://localhost:4300/remoteEntry.js',
+            exposedModule: './routes'
+        }).then(m => m.routes)
+    },
+    {
+        path: 'home',
+        loadChildren: () => loadRemoteModule({
+            type: 'module',
+            remoteEntry: 'http://localhost:4400/remoteEntry.js',
+            exposedModule: './routes'
+        }).then(m => m.routes)
+    }
+];
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const mf = require("@angular-architects/module-federation/webpack");
+const path = require("path");
+const shareAll = mf.shareAll;
+
+const sharedMappings = new mf.SharedMappings();
+sharedMappings.register(
+  path.join(__dirname, 'tsconfig.json'),
+  [/* mapped paths to share */]);
+
+module.exports = {
+  output: {
+    uniqueName: "shopFolder",
+    publicPath: "auto",
+    scriptType: "text/javascript"
+  },
+  optimization: {
+    runtimeChunk: false
+  },   
+  resolve: {
+    alias: {
+      ...sharedMappings.getAliases(),
+    }
+  },
+  experiments: {
+    outputModule: true
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+        library: { type: "module" },
+
+        // For remotes (please adjust)
+        name: "shopFolder",
+        filename: "remoteEntry.js",
+        // exposes: {
+        //     './Component': './/src/app/app.component.ts',
+        // },        
+        
+        // For hosts (please adjust)
+        remotes: {
+          "shopFolderLogin": "http://localhost:4300/remoteEntry.js",
+          "shopFolderHome": "http://localhost:4400/remoteEntry.js"
+        },
+
+        shared: {...shareAll({ singleton: true, strictVersion: true, requiredVersion: "auto" })}
+        
+    }),
+    sharedMappings.getPlugin()
+  ],
+};
