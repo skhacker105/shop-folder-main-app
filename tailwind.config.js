3149d496a82538a3c2c741dd6923fbf0e9bf3281
skhacker105 <skhacker105@gmail.com>
2024-01-14 16:31:47 +0530

    updated tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,92 +9,51 @@ module.exports = {
         "default": '11px',
         "page-header": '11px',
 
-        "dashboard-zone1-box": '9px',
-        "dashboard-zone1-title": '9px',
-        "dashboard-zone2-box": '9px',
-        "dashboard-zone2-title": '9px',
-        "dashboard-zone3-box": '9px',
-        "dashboard-zone3-title": '9px',
-        "dashboard-zone4-box": '9px',
-        "dashboard-zone4-title": '9px',
+        "dashboard-box": '9px',
+        "dashboard-title": '9px',
       },
       backgroundColor: {
         "default": 'white',
         "page-content": 'transparent',
         "dashboard": {
-          "zone1": {
-            "logo": 'transparent',
-            "box": '#6d5e00c2',
-            "title": '#6d5e00'
-          },
-          "zone2": {
-            "logo": 'transparent',
-            "box": '#550081d4',
-            "title": '#5a0a43'
-          },
-          "zone3": {
-            "logo": 'transparent',
-            "box": '#00556dc2',
-            "title": '#00556d'
-          },
-          "zone4": {
-            "logo": 'transparent',
-            "box": '#00006dc2',
-            "title": '#0000FF'
-          }
+          "logo": 'transparent',
+          "box": '#afb1c3c2',
+          "title": '#020268',
+          "title-logo": 'transparent',
         },
       },
       textColor: {
         "page-header": '#ffffff',
         "page-footer": '#ffffff',
         "dashboard": {
-          "zone1": {
-            "logo": '#ffffff',
-            "box": '#ffffff',
-            "title": '#ffffff'
-          },
-          "zone2": {
-            "box": '#ffffff',
-            "title": '#ffffff'
-          },
-          "zone3": {
-            "box": '#ffffff',
-            "title": '#ffffff'
-          },
-          "zone4": {
-            "box": '#ffffff',
-            "title": '#ffffff'
-          }
+          "box": '#000000',
+          "title": '#ffffff'
         },
       },
       colors: {
         "dashboard": {
-          "zone1": {
-            "logo": '#ffffff'
-          },
-          "zone2": {
-            "logo": '#ffffff',
-          },
-          "zone3": {
-            "logo": '#ffffff',
-          },
-          "zone4": {
-            "logo": '#ffffff',
-          }
+          "logo": 'black',
+          "title-logo": 'white'
         },
       },
       backgroundImage: {
-        "page-header": 'linear-gradient(180deg, rgb(15 28 134 / 91%) 4%, rgb(102 3 255 / 86%) 92%)',
-        "page-footer": 'linear-gradient(180deg, rgb(15 28 134 / 91%) 4%, rgb(102 3 255 / 86%) 92%)'
+        "page-header": `url('assets/bg-header.png')`,
+        "page-footer": `url('assets/bg-footer.png')`,
+        "dashboard-title": `url('assets/bg-footer.png')`,
       },
       lineHeight: {
         "page-header": '33px'
+      },
+      borderColor: {
+        "dashboard": {
+          "title": "white"
+        }
       }
     },
   },
   plugins: [],
   safelist: [{
-    pattern: /(bg|background|text|leading|fill|stroke)-(default|page|dashboard)/
+    pattern: /(bg|background|text|leading|fill|stroke|border-b)-(default|page|dashboard|url)/
   }]
 }
 
