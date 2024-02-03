b6b68913f28887af9435e64c11fca2d389e93bdf
skhacker105 <skhacker105@gmail.com>
2024-02-03 22:41:13 +0530

    added input component fixed contact groups and grid service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,10 @@ import { provideRouter } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(BrowserAnimationsModule)]
+  providers: [provideRouter(routes), importProvidersFrom(BrowserAnimationsModule), provideHttpClient(), provideToastr()]
 };
+
