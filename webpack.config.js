8a52ac1dcb8ba33a28797a6958790e5c8cdbdfff
skhacker105 <skhacker105@gmail.com>
2024-02-12 13:35:12 +0530

    updated webpack config to change remotes based on capacitor

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,22 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const mf = require("@angular-architects/module-federation/webpack");
 const path = require("path");
 const shareAll = mf.shareAll;
+const isCapBuild = process.env.CAP_BUILD;
+
+const remotes =
+  isCapBuild == null
+    // Configuration for when developing locally
+    ? {
+      shopFolderLogin: "shopFolderLogin@http://localhost:4300/remoteEntry.js",
+      shopFolderHome: "shopFolderHome@http://localhost:4400/remoteEntry.js",
+      shopFolderContact: "shopFolderContact@http://localhost:4600/remoteEntry.js"
+    }
+    // Configuration for when building for capacitor
+    : {
+      shopFolderLogin: "shopFolderLogin@shopFolderLogin/remoteEntry.js",
+      shopFolderHome: "shopFolderHome@shopFolderHome/remoteEntry.js",
+      shopFolderContact: "shopFolderContact@shopFolderContact/remoteEntry.js"
+    };
 
 const sharedMappings = new mf.SharedMappings();
 sharedMappings.register(
@@ -16,7 +32,7 @@ module.exports = {
   },
   optimization: {
     runtimeChunk: false
-  },   
+  },
   resolve: {
     alias: {
       ...sharedMappings.getAliases(),
@@ -27,24 +43,20 @@ module.exports = {
   },
   plugins: [
     new ModuleFederationPlugin({
-        library: { type: "module" },
+      library: { type: "module" },
 
-        // For remotes (please adjust)
-        name: "shopFolder",
-        filename: "remoteEntry.js",
-        // exposes: {
-        //     './Component': './/src/app/app.component.ts',
-        // },        
-        
-        // For hosts (please adjust)
-        remotes: {
-          "shopFolderLogin": "shopFolderLogin@http://localhost:4300/remoteEntry.js",
-          "shopFolderHome": "shopFolderHome@http://localhost:4400/remoteEntry.js",
-          "shopFolderContact": "shopFolderContact@http://localhost:4600/remoteEntry.js"
-        },
+      // For remotes (please adjust)
+      name: "shopFolder",
+      filename: "remoteEntry.js",
+      // exposes: {
+      //     './Component': './/src/app/app.component.ts',
+      // },        
 
-        shared: {...shareAll({ singleton: true, strictVersion: true, requiredVersion: "auto" }), 'src/assets/logo.png': { eager: true, singleton: true} }
-        
+      // For hosts (please adjust)
+      remotes,
+
+      shared: { ...shareAll({ singleton: true, strictVersion: true, requiredVersion: "auto" }), 'src/assets/logo.png': { eager: true, singleton: true } }
+
     }),
     sharedMappings.getPlugin()
   ],
